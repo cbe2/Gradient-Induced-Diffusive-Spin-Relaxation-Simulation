4d49a53ba4223f17c20aaaa3f82f0ce676a0f480
Blake Erickson <cbe2@illinois.edu>
2021-02-09 18:15:02 -0600

    Added a rectangular cell walk class with a stem and corresponding test in Examples folder.

---
+++ ExamplesAndTests/TestCellWalk.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import math
+import numpy as np
+import sys
+sys.path.append('../')
+from WalkerClasses import *
+
+np.random.seed(2)
+#random.seed(5)
+
+paramsDict={
+'D':1.0,
+'r0':[0,0,0.6],
+'dt': 1e-4,
+'L': [1.,1.,1.0],
+'SL': [0.1,0.1]
+}
+
+N=1
+steps=10000
+
+walkers=[]
+x=[]
+y=[]
+z=[]
+
+for i in range(N):
+    #paramsDict['r0']=[np.random.uniform(0,L)]
+    walkers.append(CellWalk(paramsDict))
+for i in range(steps):
+    for W in walkers:
+        x.append(W.ptcl.r[0])
+        y.append(W.ptcl.r[1])
+        z.append(W.ptcl.r[2])
+        W.step(lambda x:0)
+
+plt.plot(x,z,label="path")
+#plt.plot(y,z,label="path")
+plt.scatter(x[0],z[0],c='r',label='start')
+plt.xlabel('x')
+plt.ylabel('z')
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+# ax.plot(x, y, z, label='parametric curve')
+# ax.legend()
+plt.show()
--- WalkerClasses.py
+++ WalkerClasses.py
@@ -9,7 +9,7 @@ BoltzmannK= 1.380649e-23 #in J/K
 RoomTemp=294.3 #Kelvin (70 degrees farenheit)
 
 """
-Contains all the classes used for the random walk simulation
+Contains all the classes used for the random walk simulation. Times in seconds, lengths in cm
 """
 
 #This class serves as the particle
@@ -82,12 +82,13 @@ class SemiFreeWalk(Walk):
 
         return
 
-#Random walk in box with lengths specified by the tuple L
+#Random walk in box of arbitary dimension and lengths specified by the tuple L
+#Box is assumed to have 1 corner at the orgin and have all coordinates positive
 class BoxWalk(Walk):
     def __init__(self,paramsDict):
         Walk.__init__(self,paramsDict)
 
-        self.L=paramsDict['L'] #tuple of lengths of the box, all in cm
+        self.L=paramsDict['L'] #tuple of lengths of the box, all in cm. Length of the tuple determines the dimension of the box
 
         if len(self.L)!=len(self.ptcl.r):
             raise ValueError('Spatial dimension does not match box dimension!')
@@ -117,11 +118,104 @@ class BoxWalk(Walk):
 
         for i in range(len(self.L)):
             if self.ptcl.r[i]<self.L[i]:
-                self.ptcl.r[i]=np.abs(self.ptcl.r[i]) #relfects about x=0
+                self.ptcl.r[i]=np.abs(self.ptcl.r[i]) #relfects about origin
             if self.ptcl.r[i]>self.L[i]:
                 self.ptcl.r[i]=self.L[i]-np.abs(self.ptcl.r[i]-self.L[i]) #reflects about x=L
 
         return
+
+#3D Random walk in a rectangular cell with feed through at the top along the z-axis
+#Box+feed through is assumed be symmetric about z-axis. Feed through is assumed to be infinitely high
+class CellWalk(Walk):
+    def __init__(self,paramsDict):
+        Walk.__init__(self,paramsDict)
+
+        self.L=paramsDict['L'] #tuple of lengths of the box, all in cm. Length of the tuple determines the dimension of the box
+        self.SL=paramsDict['SL'] #stem/straw lengths (x and y lengths only, z is infinite)
+
+        if len(self.L)!=len(self.ptcl.r):
+            raise ValueError('Spatial dimension does not match box dimension!')
+
+        if len(self.ptcl.r)!=3:
+            raise ValueError('Spatial dimension must be 3!')
+
+        if np.min(self.L)<= 3.*np.sqrt(self.dt*2.*self.D):
+            raise ValueError('Step size within 3 sigma of cell wall!')
+
+        if np.min(self.SL)<= 3.*np.sqrt(self.dt*2.*self.D):
+            raise ValueError('Step size within 3 sigma of stem wall!')
+
+        print("mfp = "+str(np.sqrt(self.dt*2.*self.D)))
+
+    #steps the particle and updates particle preccesion according to Bfield
+    def step(self,Bfield):
+
+        #aquires next step vector
+        l=self.getStep()
+
+
+        #spin precesses at current position
+        self.ptcl.theta+=self.gamma*Bfield(self.ptcl.r)*self.dt
+
+        r0=self.ptcl.r.copy() #important to copy because of mutable aliasing
+        self.ptcl.r+=l
+        self.ptcl.steps+=int(1)
+        self.reflect(r0)
+
+        return
+
+    #checks to see if walker is in stem
+    def inStem(self,r):
+        if np.abs(r[0])<0.5*self.SL[0] and np.abs(r[1])<0.5*self.SL[1]: return True
+        else: return False
+
+    #Reflects particle according specular reflection rule
+    def reflect(self,r0):
+
+        #reflect about x and y axis
+        for i in range(2):
+            if self.ptcl.r[i]> 0.5*self.L[i]:
+                self.ptcl.r[i]=self.L[i]-self.ptcl.r[i]
+            if self.ptcl.r[i]< -0.5*self.L[i]:
+                self.ptcl.r[i]=-self.L[i]-self.ptcl.r[i]
+
+        #reflect about bottom z
+        if self.ptcl.r[2]< -0.5*self.L[2]:
+            self.ptcl.r[2]=-self.L[2]-self.ptcl.r[2]
+
+        #reflect about stem if both points are above cell
+        if r0[2]> 0.5*self.L[2] and self.ptcl.r[2]> 0.5*self.L[2]:
+            #reflect off stem walls if needed
+            for i in range(2):
+                if self.ptcl.r[i]> 0.5*self.SL[i]:
+                    self.ptcl.r[i]=self.SL[i]-self.ptcl.r[i]
+                if self.ptcl.r[i]< -0.5*self.SL[i]:
+                    self.ptcl.r[i]=-self.SL[i]-self.ptcl.r[i]
+
+        if (r0[2]< 0.5*self.L[2] and self.ptcl.r[2]> 0.5*self.L[2]) or (r0[2]> 0.5*self.L[2] and self.ptcl.r[2]< 0.5*self.L[2]):
+            #if the particle remains in the stem, do nothing
+            if self.inStem(r0) and self.inStem(self.ptcl.r):
+                #print("Still in stem")
+                return
+
+            # if passing cell boundary out of stem, reflect off of cell
+            elif (not self.inStem(r0)) and (not self.inStem(self.ptcl.r)):
+                #print("not in stem")
+                self.ptcl.r[2]=self.L[2]-self.ptcl.r[2]
+
+            # if passing cell boundary but into the stem do nothing (to be fixed)
+            elif (not self.inStem(r0)) and (self.inStem(self.ptcl.r)):
+                #print("entering stem")
+                return
+
+            # if passing cell boundary and out the stem do nothing (to be fixed)
+            elif (self.inStem(r0)) and (not self.inStem(self.ptcl.r)):
+                #print("leaving stem")
+                return
+
+        return
+
+
 
 
 
